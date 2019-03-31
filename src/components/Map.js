d9672904bcb8098d962c0012b134749bdab92bad
usedtobemeonce <seven.eightm5@gmail.com>
2019-03-31 23:00:51 +0200

    Add delete pin

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
-import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
+import ReactMapGL, { NavigationControl, Marker, Popup } from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
 import differenceInMinutes from 'date-fns/difference_in_minutes';
-// import Button from "@material-ui/core/Button";
-// import Typography from "@material-ui/core/Typography";
-// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import Button from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import Typography from "@material-ui/core/Typography";
 
 import { useClient } from '../client';
 import { GET_PINS_QUERY } from '../graphql/queries';
+import { DELETE_PIN_MUTATION } from '../graphql/mutations';
 import PinIcon from './PinIcon';
 import Blog from './Blog';
 import Context from '../context';
@@ -23,6 +24,7 @@ const Map = ({ classes }) => {
   const { state, dispatch } = useContext(Context);
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
   const [userPosition, setUserPosition] = useState(null);
+  const [popup, setPopup] = useState(null);
 
   useEffect(() => {
     getUserPosition();
@@ -65,6 +67,20 @@ const Map = ({ classes }) => {
   const highlightNewPin = pin => {
     const isNewPin = differenceInMinutes(Date.now(), Number(pin.createdAt)) <= 30;
     return isNewPin ? 'limegreen' : 'darkblue';
+  }
+
+  const handleSelectPin = pin => {
+    setPopup(pin);
+    dispatch({ type: 'SET_PIN', payload: pin });
+  }
+
+  const isAuthUser = () => state.currentUser._id === popup.author._id;
+
+  const handleDeletePin = async pin => {
+    const variables = { pinId: pin._id }
+    const { deletePin } = await client.request(DELETE_PIN_MUTATION, variables);
+    dispatch({ type: 'DELETE_PIN', payload: deletePin });
+    setPopup(null);
   }
 
   return (
@@ -113,9 +129,37 @@ const Map = ({ classes }) => {
             offsetLeft={-19}
             offsetTop={-37}
           >
-            <PinIcon size={40} color={highlightNewPin(pin)} />
+            <PinIcon onClick={() => handleSelectPin(pin)} size={40} color={highlightNewPin(pin)} />
           </Marker>
         ))}
+
+        {/* Popup dialog for the created pins */}
+        {popup && (
+          <Popup
+            anchor='top'
+            latitude={popup.latitude}
+            longitude={popup.longitude}
+            closeOnClick={false}
+            onClose={() => setPopup(null)}
+          >
+            <img
+              className={classes.popupImage}
+              src={popup.image}
+              alt={popup.title}
+            />
+            <div className={classes.popupTab}>
+              <Typography>
+                {popup.latitude.toFixed(6)}, {popup.longitude.toFixed(6)}
+              </Typography>
+              {isAuthUser() && (
+                <Button onClick={() => handleDeletePin(popup)}>
+                  <DeleteIcon className={classes.deleteIcon} />
+                </Button>
+              )}
+            </div>
+          </Popup>
+        )}
+
       </ReactMapGL>
 
       <Blog />
--- src/reducer.js
+++ src/reducer.js
@@ -22,7 +22,8 @@ export default function reducer(state, { type, payload }) {
                 draft: {
                     latitude: 0,
                     longitude: 0
-                }
+                },
+                currentPin: null,
             };
         case 'UPDATE_DRAFT_LOCATION':
             return {
@@ -45,6 +46,20 @@ export default function reducer(state, { type, payload }) {
             return {
                 ...state,
                 pins: [...prevPins, newPin]
+            }
+        case 'SET_PIN':
+            return {
+                ...state,
+                currentPin: payload,
+                draft: null
+            }
+        case 'DELETE_PIN':
+            const deletedPin = payload;
+            const filteredPins = state.pins.filter(pin => pin._id !== deletedPin._id);
+            return {
+                ...state,
+                pins: filteredPins,
+                currentPin: null
             }
         default: {
             return state;
